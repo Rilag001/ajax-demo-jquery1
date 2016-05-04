42e3baeb146dc2ff14487370be1ecf32cf8f58fa
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-04 11:21:42 +0200

    Update jquery-ajax.js

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -8,13 +8,14 @@ $('#my-button').click(function() {
   
 $.get('http://api.spotify.com/v1/users/rickylagerkvist', function(data, textStatus, jqXHR) {
   
-  // Visa texten från servern i stycket (<p>) med id:t show-server-text
-  $('#show-server-text').text(data);
-  
   // Vilken status
   console.log(textStatus);
-} )
+  console.log(jqXHR);
   
-  
-} );
+  // Visa texten från servern i stycket (<p>) med id:t show-server-text
+  $('#show-server-text').text(data);
+
+} ).fail(function() { console.log("Request gav fel!")})
+} 
+);
 
