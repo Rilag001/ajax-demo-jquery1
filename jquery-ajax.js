33e35ba72ae515bc3881dd5ff63d68473cdc5f4a
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-04 11:00:00 +0200

    Update jquery-ajax.js

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -6,7 +6,7 @@ $('#my-button').click(function() {
   console.log("Knappen klickades på!") 
   
   
-$.get('https://api.github.com/zen', function(data, textStatus, jqXHR) {
+$.get('http://feber.se/video/', function(data, textStatus, jqXHR) {
   
   // Visa texten från servern i stycket (<p>) med id:t show-server-text
   $('#show-server-text').text(data);
