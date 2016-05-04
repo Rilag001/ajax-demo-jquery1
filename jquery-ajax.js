45091030881cf23fa700cfe4c6169bf5ae1d0e4b
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-04 10:43:44 +0200

    Update jquery-ajax.js

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -3,3 +3,12 @@
 // Använd jQuery och AJAX
 
 $('#my-button').click(function() { console.log("Knappen klickades på!") } );
+
+$.get("http://feber.se/video/", function(data, textstatus, jqxHR) {
+  
+  // Visa texten från servern i stycket (<p>) med id:t show-server-text
+  $('#show-server-text').text(data);
+  
+  // Vilken status
+  console.log(textstatus);
+} )
